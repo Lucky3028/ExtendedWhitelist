55f5b9d21cdd2c0d3484f0d3564ec633e960296f
lucky3028 <genkana7882@gmail.com>
2020-05-22 22:52:18 +0900

    [Remove] 不要なImportを削除

--- src/main/kotlin/com/lucky3028/extendedwhitelist/utils/Message.kt
+++ src/main/kotlin/com/lucky3028/extendedwhitelist/utils/Message.kt
@@ -1,7 +1,6 @@
 package com.lucky3028.extendedwhitelist.utils
 
 import org.bukkit.Bukkit
-import org.bukkit.ChatColor
 import org.bukkit.command.CommandSender
 
 fun logInfo(msg: String) {Bukkit.getLogger().info(msg)}
